bbb388f297cf5268914a334e9f42a32aa8bc237d
andreiggr <andrei.ggr@gmail.com>
2019-10-26 02:46:54 +0300

    added dynamic circular icon

--- src/components/HabitCard.js
+++ src/components/HabitCard.js
@@ -1,80 +1,130 @@
-import React, { Component } from 'react';
-import { Paper, Typography, CircularProgress, IconButton } from '@material-ui/core';
+import React, { Component, useState } from "react";
+import {
+  Paper,
+  Typography,
+  CircularProgress,
+  IconButton
+} from "@material-ui/core";
 import { Close, Check } from "@material-ui/icons";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import moment from "moment";
 import _ from "lodash";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import nextDayId from '../utils/nextDayId';
+import nextDayId from "../utils/nextDayId";
 import { week } from "../utils/constants";
 
-import { habitAddDate, habitRemoveDate, habitSelect } from '../actions/habits';
+import { habitAddDate, habitRemoveDate, habitSelect } from "../actions/habits";
 
+const HabitCard = ({
+  habit,
+  habits,
+  addDate,
+  removeDate,
+  habitSelect,
+  selectedHabit
+}) => {
+  //const [weekCompletion, setWeekCompletion] = useState(0);
 
+  const handleCheck = day => {
+    const habitId = habit.id;
+    const id = nextDayId(habits);
+    const date = moment(day).unix();
+    const newDay = { id, habitId, date };
+    addDate(newDay);
+  };
 
-const HabitCard = ({ habit, habits, addDate, removeDate, habitSelect, selectedHabit }) => {
+  const handleUnCheck = day => {
+    const date = moment(day).unix();
+    var habitDays = habit.days;
+    var removedDay = habitDays.filter(
+      day => Math.floor(day.date / 10000) === Math.floor(date / 10000)
+    )[0];
+    removeDate(removedDay);
+  };
 
-    const handleCheck = (day) => {
-        const habitId = habit.id;
-        const id = nextDayId(habits)
-        const date = moment(day).unix();
-        const newDay = { id, habitId, date };
-        addDate(newDay);
-    }
+  const handleSelect = () => {
+    habitSelect(habit);
+  };
 
-    const handleUnCheck = (day) => {
-        const date = moment(day).unix();
-        var habitDays = habit.days;
-        var removedDay = habitDays.filter(day => Math.floor(day.date / 10000) === Math.floor(date / 10000))[0];
-        removeDate(removedDay);
-    }
+  const title = habit.title;
+  const habitDays = habit.days.map(day =>
+    moment.unix(day.date).format("ddd Do")
+  );
 
-    const handleSelect = () => {
-        habitSelect(habit)
-    }
+  const daysChecked = week
+    .map(day => habitDays.includes(day.format("ddd Do")))
+    .filter(Boolean).length;
+  const weekCompletion = (daysChecked * 100) / 5;
 
-    const title = habit.title;
-    const habitDays = habit.days.map(day => moment.unix(day.date).format("ddd Do"));
-
-    return (
-
-        <div>
-            <Paper style={{ marginTop: "10px", padding: "5px", display: "flex", flexDirection: "row", alignItems: "center", background: "#EFEFEF" }}>
-                <CircularProgress variant="static" value={100} size={25} style={{ margin: "3px", marginRight: "10px" }} />
-                <div style={{ width: "125px" }}>
-                    <Link style={{ textDecoration: "none" }} to={`/habit/${title}`} >
-                    <Typography variant="h6" style={{ cursor: "pointer", color: "black" }} onClick={() => handleSelect()} >
-                        {title}
-                    </Typography>
-                    </Link>
-                </div>
-                {week.map((day, index) =>
-                    <div key={index} style={{ marginLeft: "30px", marginRight: "47px", cursor: "pointer" }}>
-                        {habitDays.includes(day.format("ddd Do")) && <Check onClick={() => handleUnCheck(day)} color="primary" />}
-                        {!habitDays.includes(day.format("ddd Do")) && <Close onClick={() => handleCheck(day)} color="disabled" />}
-                    </div>)}
-            </Paper>
+  return (
+    <div>
+      <Paper
+        style={{
+          marginTop: "10px",
+          padding: "5px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          background: "#EFEFEF"
+        }}
+      >
+        <CircularProgress
+          variant="static"
+          value={weekCompletion}
+          size={25}
+          style={{ margin: "3px", marginRight: "10px" }}
+        />
+        <div style={{ width: "125px" }}>
+          <Link style={{ textDecoration: "none" }} to={`/habit/${title}`}>
+            <Typography
+              variant="h6"
+              style={{ cursor: "pointer", color: "black" }}
+              onClick={() => handleSelect()}
+            >
+              {title}
+            </Typography>
+          </Link>
         </div>
-    );
-}
+        {week.map((day, index) => (
+          <div
+            key={index}
+            style={{
+              marginLeft: "30px",
+              marginRight: "47px",
+              cursor: "pointer"
+            }}
+          >
+            {habitDays.includes(day.format("ddd Do")) && (
+              <Check onClick={() => handleUnCheck(day)} color="primary" />
+            )}
+            {!habitDays.includes(day.format("ddd Do")) && (
+              <Close onClick={() => handleCheck(day)} color="disabled" />
+            )}
+          </div>
+        ))}
+      </Paper>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => {
-    return {
-        habits: state.habits,
-        selectedHabit: state.selectedHabit
-    }
-}
+const mapStateToProps = state => {
+  return {
+    habits: state.habits,
+    selectedHabit: state.selectedHabit
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addDate: (date) => dispatch(habitAddDate(date)),
-        removeDate: (date) => dispatch(habitRemoveDate(date)),
-        habitSelect: (habit) => dispatch(habitSelect(habit))
-    };
+const mapDispatchToProps = dispatch => {
+  return {
+    addDate: date => dispatch(habitAddDate(date)),
+    removeDate: date => dispatch(habitRemoveDate(date)),
+    habitSelect: habit => dispatch(habitSelect(habit))
+  };
+};
 
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(HabitCard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HabitCard);
